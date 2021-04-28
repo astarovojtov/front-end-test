152315e190aa4828787755c65e7b79fa57a156c8
UladSasnouski <70658862+UladSasnouski@users.noreply.github.com>
2021-04-28 22:45:20 +0300

    feat: fix bags

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ let inputPhone = document.getElementById("inputPhone");
 let submit = document.getElementById("submit");
 let output = document.getElementById('output');
 var contact = document.createElement('div');
-let count = 0;
+var errorMessage = document.getElementById('error-message');
 let checked = false;
 let edited = false;
+let localPhone;
 let users = [];
 
 submit.addEventListener("click", function () {
@@ -18,48 +19,73 @@ function Check(name, phone) {
         let controlName = /^(?:((([^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]'’,\-.\s])){1,}(['’,\-\.]){0,1}){2,}(([^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]'’,\-. ]))*(([ ]+){0,1}(((([^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]'’,\-\.\s])){1,})(['’\-,\.]){0,1}){2,}((([^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]'’,\-\.\s])){2,})?)*)$/;
         let controlPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
         if (name == "" || phone == "") {
-            alert("Пожалуйста, введите имя или телефон");
+            errorMessage.classList.add('open-error');
+            let textError = document.getElementById('textError');
+            textError.innerHTML = 'Enter Name or Phone';
             return false;
         };
         if (!controlName.test(name)) {
-            alert('Имя не соответствует');
+            errorMessage.classList.add('open-error');
+            let textError = document.getElementById('textError');
+            textError.innerHTML = 'Invalid Name';
             return false;
         };
         if (!controlPhone.test(phone)) {
-            alert('Телефон не соответствует');
+            errorMessage.classList.add('open-error');
+            let textError = document.getElementById('textError');
+            textError.innerHTML = 'Invalid Phone';
             return false;
         };
         if (users.length != 0) {
             let userIndex = users.findIndex(user => user.phone === phone)
             if (userIndex != -1) {
-                alert('Телефон уже существует');
+                errorMessage.classList.add('open-error');
+                let textError = document.getElementById('textError');
+                textError.innerHTML = 'Phone already exists';
                 return false;
             };
+        }
+        if (errorMessage.classList.contains('open-error')) {
+            errorMessage.classList.remove('open-error');
+        } else {
+            return 0;
         }
         checked = true;
     } else {
         let controlName = /^(?:((([^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]'’,\-.\s])){1,}(['’,\-\.]){0,1}){2,}(([^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]'’,\-. ]))*(([ ]+){0,1}(((([^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]'’,\-\.\s])){1,})(['’\-,\.]){0,1}){2,}((([^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]'’,\-\.\s])){2,})?)*)$/;
         let controlPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
         if (inputName.value == "" || inputPhone.value == "") {
-            alert("Пожалуйста, введите имя или телефон");
+            errorMessage.classList.add('open-error');
+            let textError = document.getElementById('textError');
+            textError.innerHTML = 'Enter Name or Phone';
             return false;
         };
         if (!controlName.test(inputName.value)) {
-            alert('Имя не соответствует');
+            errorMessage.classList.add('open-error');
+            let textError = document.getElementById('textError');
+            textError.innerHTML = 'Invalid Name';
             return false;
         };
         if (!controlPhone.test(inputPhone.value)) {
-            alert('Телефон не соответствует');
+            errorMessage.classList.add('open-error');
+            let textError = document.getElementById('textError');
+            textError.innerHTML = 'Invalid Phone';
             return false;
         };
         if (users.length != 0) {
             let userIndex = users.findIndex(user => user.phone === inputPhone.value)
             if (userIndex != -1) {
-                alert('Телефон уже существует');
+                errorMessage.classList.add('open-error');
+                let textError = document.getElementById('textError');
+                textError.innerHTML = 'Phone already exists';
                 return false;
             };
         }
-
+        if (errorMessage.classList.contains('open-error')) {
+            errorMessage.classList.remove('open-error');
+        } else {
+            return 0;
+        }
         checked = true;
     }
 }
@@ -67,18 +93,20 @@ function Check(name, phone) {
 
 output.onclick = function (event) {
     let target = event.target.textContent;
-    let targetParent = event.target.parentNode.parentNode.id;
+    let targetParent = event.target.parentNode.parentNode;
     let targetParentChildrenName = event.target.parentNode.parentNode.childNodes[1];
     let targetParentChildrenPhone = event.target.parentNode.parentNode.childNodes[3];
+    if (edited === false && targetParentChildrenPhone.textContent) {
+        localPhone = targetParentChildrenPhone.textContent;
+    }
     switch (target) {
         case 'delete':
-            let del = document.getElementById(`${targetParent}`);
-            console.log(targetParent)
-            del.classList.add("destroy");
+            targetParent.classList.add("destroy");
+            let userIndex = users.findIndex(user => user.phone === targetParentChildrenPhone.textContent)
             setTimeout(function () {
-                users.splice(targetParent, 1);
+                users.splice(userIndex, 1);
                 updateLocal();
-                del.remove();
+                targetParent.remove();
             }, 900);
             break;
         case 'edit':
@@ -90,15 +118,15 @@ output.onclick = function (event) {
             event.target.textContent = 'save';
             break;
         case 'save':
-            console.log(targetParent)
             Check(targetParentChildrenName.textContent, targetParentChildrenPhone.textContent);
             if (checked === true) {
                 targetParentChildrenName.classList.remove("edit");
                 targetParentChildrenPhone.classList.remove("edit");
                 targetParentChildrenName.contentEditable = false;
                 targetParentChildrenPhone.contentEditable = false;
-                users[targetParent].name = targetParentChildrenName.textContent;
-                users[targetParent].phone = targetParentChildrenPhone.textContent;
+                let userIndex = users.findIndex(user => user.phone === localPhone)
+                users[userIndex].name = targetParentChildrenName.textContent;
+                users[userIndex].phone = targetParentChildrenPhone.textContent;
                 event.target.textContent = 'edit';
                 updateLocal();
                 checked = false;
@@ -113,7 +141,6 @@ output.onclick = function (event) {
 
 class Contacts {
     constructor(object) {
-        this.id = object.id;
         this.name = object.name;
         this.phone = object.phone;
     }
@@ -121,7 +148,7 @@ class Contacts {
 
 function CreateContact() {
     if (checked === true) {
-        var str = `<div class='grid-container' id='${count}'>
+        var str = `<div class='grid-container'>
     <div class="name" contenteditable="false">${inputName.value}</div>
     <div class="phone" contenteditable="false">${inputPhone.value}</div>
     <div class="btn-edit"><button>edit</button></div>
@@ -130,11 +157,9 @@ function CreateContact() {
         contact.innerHTML = str + contact.innerHTML;
 
         let user = new Contacts({
-            id: count,
             name: inputName.value,
             phone: inputPhone.value,
         })
-        count++;
         output.append(contact)
         users.push(user);
         inputName.value = "";
@@ -154,18 +179,8 @@ function init() {
         users = JSON.parse(localStorage.getItem('storedUsers'));
     }
 
-    users.sort(function (a, b) {
-        if (a.id > b.id) {
-            return 1;
-        }
-        if (a.id < b.id) {
-            return -1;
-        }
-        return 0;
-    });
-
     for (var i = 0; i < users.length; i++) {
-        var str = `<div class='grid-container' id='${users[i].id}'>
+        var str = `<div class='grid-container'>
     <div class="name" contenteditable="false">${users[i].name}</div>
     <div class="phone" contenteditable="false">${users[i].phone}</div>
     <div class="btn-edit"><button>edit</button></div>
@@ -174,11 +189,9 @@ function init() {
         contact.innerHTML = str + contact.innerHTML;
 
         let user = new Contacts({
-            id: users[i].id,
             name: users[i].name,
             phone: users[i].phone,
         })
-        count = users.length;
         output.append(contact)
     }
 }
